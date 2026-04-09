5d367d7df5a92801a7ca2d3072b96006ae0d0a11
Paras Kalyan <paraskalyan20@gmail.com>
2026-04-09 10:26:22 +0530

    Initial Setup

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import authRoutes from './modules/auth/auth.route.js';
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, ()=>{
+    console.log("Server Running")
+})
---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from "express";
+
+export const signup = (req: Request, res: Response)=>{
+
+}
+
+export const login = (req: Request, res: Response)=>{
+
+}
+
+export const logout = (req: Request, res: Response)=>{
+
+}
