93efdd7791c739f5def5e2d03d204e8f8af5a7fd
Paras Kalyan <paraskalyan20@gmail.com>
2026-04-13 21:30:25 +0530

    Login logic

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -30,7 +30,37 @@ export const signup = (req: Request, res: Response)=>{
 }
 
 export const login = (req: Request, res: Response)=>{
-
+    const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and password are required" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const { accessToken, refreshToken } = generateToken(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+    setCookies(res, accessToken, refreshToken);
+    res.status(200).json({
+      message: "Login successful",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
 }
 
 export const logout = (req: Request, res: Response)=>{
