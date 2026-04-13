d96ef05ff72575916603363fd4d201142c3bad3c
Paras Kalyan <paraskalyan20@gmail.com>
2026-04-13 21:29:53 +0530

    Signup logic

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,7 +1,32 @@
 import type { Request, Response } from "express";
 
 export const signup = (req: Request, res: Response)=>{
+    const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+  try {
+    const newUser = await User.create({ name, email, password });
+    const { accessToken, refreshToken } = generateToken(newUser._id);
+    await storeRefreshToken(newUser._id, refreshToken);
+    setCookies(res, accessToken, refreshToken);
 
+    res.status(201).json({
+      message: "User created successfully",
+      user: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
 }
 
 export const login = (req: Request, res: Response)=>{
