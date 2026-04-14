06ef5e54233bcf0f69170f7ce6e49745e20bb0c4
Paras Kalyan <paraskalyan20@gmail.com>
2026-04-14 10:57:34 +0530

    Global Error handling backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import authRoutes from './modules/auth/auth.routes.js';
+import { errorHandler } from './middlewares/error.middleware.js';
+import { AppError } from './utils/AppError.js';
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+
+app.use((req, res, next) => {
+  next(new AppError(`Route ${req.originalUrl} not found`, 404));
+});
+app.use(errorHandler);
 
 app.listen(PORT, ()=>{
     console.log("Server Running")
---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,22 @@
+import type {Request, Response, NextFunction } from 'express';
+import { AppError } from '../utils/AppError';
+
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Internal Server Error';
+
+  if (err.name === 'ValidationError') {
+    statusCode = 400;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+  });
+};
