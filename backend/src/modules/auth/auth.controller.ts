b4d7905c11413413518fa6404cfe9da4b4bdb1e7
Paras Kalyan <paraskalyan20@gmail.com>
2026-04-14 10:12:11 +0530

    Auth Controller and Service setup

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,68 +1,23 @@
 import type { Request, Response } from "express";
+import * as authService from './auth.service.js';
 
-export const signup = (req: Request, res: Response)=>{
-    const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    return res.status(400).json({ message: "User already exists" });
-  }
+export const signup = async(req: Request, res: Response) => {
   try {
-    const newUser = await User.create({ name, email, password });
-    const { accessToken, refreshToken } = generateToken(newUser._id);
-    await storeRefreshToken(newUser._id, refreshToken);
-    setCookies(res, accessToken, refreshToken);
+    const result = await authService.signup();
+    res.json(result);
+  } catch (error) {  }
+};
 
-    res.status(201).json({
-      message: "User created successfully",
-      user: {
-        id: newUser._id,
-        name: newUser.name,
-        email: newUser.email,
-        role: newUser.role,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
-  }
-}
-
-export const login = (req: Request, res: Response)=>{
-    const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({ message: "Email and password are required" });
-  }
+export const login = async(req: Request, res: Response) => {
   try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ message: "Invalid email or password" });
-    }
-    const isMatch = await user.comparePassword(password);
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid email or password" });
-    }
-    const { accessToken, refreshToken } = generateToken(user._id);
-    await storeRefreshToken(user._id, refreshToken);
-    setCookies(res, accessToken, refreshToken);
-    res.status(200).json({
-      message: "Login successful",
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role,
-      },
-    });
-  } catch (error) {
-    console.error("Login error:", error);
-    return res
-      .status(500)
-      .json({ message: "Server error", error: error.message });
-  }
-}
+    const result = await authService.login();
+    res.json(result);
+  } catch (error) {}
+};
 
-export const logout = (req: Request, res: Response)=>{
-
-}
+export const logout = async(req: Request, res: Response) => {
+  try {
+    const result = await authService.logout();
+    res.json(result);
+  } catch (error) {}
+};
---
+++ backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,11 @@
+export const signup = async()=>{
+
+}
+
+export const login = async()=>{
+
+}
+
+export const logout = async()=>{
+
+}
