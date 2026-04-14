1c8b861149d0417e48a838aae30ac03a93761b79
Paras Kalyan <paraskalyan20@gmail.com>
2026-04-14 09:53:01 +0530

    Tanstack Query Setup

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function Providers({ children }: Props) {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
