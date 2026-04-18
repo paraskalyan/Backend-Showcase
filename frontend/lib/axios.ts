53e37497a4cb139a1d44c1c298ccaf7cd928751c
Paras Kalyan <paraskalyan20@gmail.com>
2026-04-18 23:59:36 +0530

    apiCLient

---
+++ frontend/lib/axios.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = "http://localhost:4000/api"
+
+const apiClient = axios.create(
+    {
+        baseURL: BASE_URL,
+        timeout: 5000,
+        withCredentials: true,
+    }
+)
+
+export default apiClient;
