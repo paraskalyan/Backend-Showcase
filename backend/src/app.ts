c4d25f8e0f70ce6c0988ae4c2265d040e28e7ddc
Paras Kalyan <paraskalyan20@gmail.com>
2026-04-22 23:33:43 +0530

    Add project and endpoint routes to the main application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import authRoutes from './modules/auth/auth.routes.js';
+import projectRoutes from './modules/projects/projects.routes.js';
+import endpointRoutes from './modules/endpoint/endpoint.routes.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 import { AppError } from './utils/AppError.js';
 
@@ -8,6 +10,8 @@ const PORT = 4000;
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/project',projectRoutes);
+app.use('/api/endpoint', endpointRoutes);
 
 app.use((req, res, next) => {
   next(new AppError(`Route ${req.originalUrl} not found`, 404));
