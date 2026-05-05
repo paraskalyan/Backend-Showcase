e69c009c3fb2e0cb855aa9b36c370f4825abe8d8
Paras Kalyan <paraskalyan20@gmail.com>
2026-05-05 22:38:39 +0530

    Add createProject function to ProjectAPIService for project creation

--- frontend/API/ProjectAPIService.ts
+++ frontend/API/ProjectAPIService.ts
@@ -1,7 +1,12 @@
 import { API_ROUTES } from "@/constants/routes"
+import { Project } from "@/constants/types";
 import apiClient from "@/lib/axios"
 
 export const getAllProjects = async () =>{
    const response = await apiClient.get(API_ROUTES.PROJECTS);
    return response.data.data;
+}
+
+export const createProject = async (data: Project)=>{
+   const response = await apiClient.post(API_ROUTES.PROJECTS, data)
 }
