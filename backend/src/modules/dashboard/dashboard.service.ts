b96311fb4643f0ef29da366fab64b6018006435a
Paras Kalyan <paraskalyan20@gmail.com>
2026-05-10 23:50:48 +0530

    Refactor getDashboardStats to accept userId and calculate project statistics

--- backend/src/modules/dashboard/dashboard.service.ts
+++ backend/src/modules/dashboard/dashboard.service.ts
@@ -1,3 +1,39 @@
-export const getDashboardStats = async () => {
+import { prisma } from "../../lib/prisma.js";
 
-}
+export const getDashboardStats = async (userId: string) => {
+  const totalProjects = await prisma.project.count({
+    where: {
+      userId,
+    },
+  });
+
+  const totalEndpoints = await prisma.endpoint.count({
+    where: {
+      Project: { userId },
+    },
+  });
+
+  const successfulExecutions = await prisma.execution.count({
+    where: {
+      status: "SUCCESS",
+      endpoint: {
+        project: {
+          userId,
+        },
+      },
+    },
+  });
+
+  const totalExecutions = await prisma.execution.count({
+    where: {
+      Endpoint: {
+        Project: {
+          userId,
+        },
+      },
+    },
+  });
+
+  const successRate =
+    totalExecutions === 0 ? 0 : (successfulExecutions / totalExecutions) * 100;
+};
