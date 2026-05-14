54a055716ed9cab2c784c6f9990f88cb3024173a
Paras Kalyan <paraskalyan20@gmail.com>
2026-05-14 23:01:43 +0530

    Enhance middleware to redirect authenticated users from login and signup to dashboard

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -6,6 +6,9 @@ export function middleware(req: NextRequest) {
   if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
+  if(token && (req.nextUrl.pathname === '/' || req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/signup")){
+    return NextResponse.redirect(new URL("/dashboard", req.url))
+  }
 
   return NextResponse.next();
 }
