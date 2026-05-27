7138be8ec10df4f320247362899c7cfee3ecc8ae
Paras Kalyan <paraskalyan20@gmail.com>
2026-05-27 23:55:44 +0530

    Refactor API service functions for consistency and formatting

--- frontend/API/EndpointAPIService.ts
+++ frontend/API/EndpointAPIService.ts
@@ -1,17 +1,24 @@
 import { API_ROUTES } from "@/constants/routes";
 import apiClient from "@/lib/axios";
 
-export const createEndpoint = async (data: any)=>{
-   const response = await apiClient.post(API_ROUTES.ENDPOINT, data);
-   return response.data.data;
-}
+export const createEndpoint = async (data: any) => {
+  const response = await apiClient.post(API_ROUTES.ENDPOINT, data);
+  return response.data.data;
+};
 
-export const getEndpoints = async (id: string)=> {
-    const response = await apiClient.get(`${API_ROUTES.ENDPOINT}?projectId=${id}`);
-    return response.data;
-}
+export const getEndpoints = async (id: string) => {
+  const response = await apiClient.get(
+    `${API_ROUTES.ENDPOINT}?projectId=${id}`,
+  );
+  return response.data;
+};
 
-export const deleteEndpoint = async (id: string)=>{
-   const response = await apiClient.delete(`${API_ROUTES.ENDPOINT}/${id}`);
-   return response.data;
-}
+export const getSingleEndpoint = async (id: string) => {
+  const response = await apiClient.get(`${API_ROUTES.ENDPOINT}/${id}`);
+  return response.data;
+};
+
+export const deleteEndpoint = async (id: string) => {
+  const response = await apiClient.delete(`${API_ROUTES.ENDPOINT}/${id}`);
+  return response.data;
+};
