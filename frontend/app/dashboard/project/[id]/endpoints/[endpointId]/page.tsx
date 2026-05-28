2dbf13706de8bca5d44d0235a1dac29cd632850c
Paras Kalyan <paraskalyan20@gmail.com>
2026-05-28 23:55:43 +0530

    Refactor EndpointDetailPage to remove unused PageProps interface

--- frontend/app/dashboard/project/[id]/endpoints/[endpointId]/page.tsx
+++ frontend/app/dashboard/project/[id]/endpoints/[endpointId]/page.tsx
@@ -1,16 +1,13 @@
+"use client";
 import { getSingleEndpoint } from "@/API/EndpointAPIService";
 import { EndpointDetailView } from "@/components/project/endpoint-detail-view";
 import { mockEndpoint } from "@/helpers/mockData";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 
-interface PageProps {
-  params: Promise<{ id: string; endpointId: string }>;
-}
-
 export default function EndpointDetailPage() {
   const { endpointId } = useParams();
-  
+
   const { data, isLoading } = useQuery({
     queryKey: ["project", endpointId],
     queryFn: () => getSingleEndpoint(String(endpointId)),
