9bbeccee2e5350d6a6393608b1c84664b202a28d
Paras Kalyan <paraskalyan20@gmail.com>
2026-07-03 00:53:33 +0530

    Remove console log of DATABASE_URL for security and clarity

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -3,7 +3,6 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
-console.log(process.env.DATABASE_URL);
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
